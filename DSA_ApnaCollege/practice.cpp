afc1372ddf5e8e42db21852cfc5cbb08415eba34
Shreyash0368 <shreyashgpt0503@gmail.com>
2022-10-21 09:18:06 +0530

    doubt in addbinary

--- DSA_ApnaCollege/practice.cpp
+++ DSA_ApnaCollege/practice.cpp
@@ -44,7 +44,7 @@ int decimalToBinary(int dec)
         binary.push_back(x);
         dec /= 2;
     }
-    
+
     std ::reverse(binary.begin(), binary.end());
     int bin = 0;
 
@@ -54,7 +54,7 @@ int decimalToBinary(int dec)
         bin *= 10;
     }
     bin /= 10;
-    
+
     return bin;
 }
 
@@ -67,7 +67,7 @@ int decimalToOctal(int dec)
         octal.push_back(x);
         dec /= 8;
     }
-    
+
     std ::reverse(octal.begin(), octal.end());
     int oct = 0;
 
@@ -77,50 +77,154 @@ int decimalToOctal(int dec)
         oct *= 10;
     }
     oct /= 10;
-    
+
     return oct;
 }
 
-std :: string decimalToHexadecimal(int dec) {
-    std :: string hex;
+std ::string decimalToHexadecimal(int dec)
+{
+    std ::string hex;
     while (dec > 0)
     {
         int x = dec % 16;
-        std :: string x2;
-        if(x >= 10) 
+        std ::string x2;
+        if (x >= 10)
             x2 = char(int((x - 10) + 'A'));
         else if (x < 10)
-            x2 = std :: to_string(x);
+            x2 = std ::to_string(x);
         hex += x2;
-        dec /= 16;        
+        dec /= 16;
     }
-    
-    std :: reverse(hex.begin(), hex.end());
-    
+
+    std ::reverse(hex.begin(), hex.end());
 
     // for (int i = 0; i < hexadec.size(); i++)
     // {
     //     hex.push_back(hexadec[i]);
     // }
 
-    return hex;    
+    return hex;
+}
+
+int addBinary(int a, int b)
+{
+    int carry = 0;
+    int tempSum = 0;
+    int a1, b1;
+
+    while (a > 0 && b > 0)
+    {
+        a1 = a % 2;
+        b1 = b % 2;
+
+        if (a1 == 0 && b1 == 0)
+        {
+            tempSum = (tempSum * 10) + carry;
+            carry = 0;
+        }
+        else if ((a1 == 1 && b1 == 0) || (a1 == 0 && b1 == 1))
+        {
+            if (carry == 1)
+            {
+                tempSum = tempSum * 10 + 0;
+                carry = 1;
+            }
+            else if (carry == 0)
+            {
+                tempSum = tempSum * 10 + 1;
+                carry = 0;
+            }
+        }
+        else
+        {
+            tempSum = tempSum * 10 + carry;
+            carry = 1;
+        }
+
+        a /= 10;
+        b /= 10;
+    }
+
+    // if a remains
+    while (a > 0)
+    {
+        if (carry == 1)
+        {
+            if (a % 2 == 1)
+            {
+                tempSum = tempSum * 10 + 0;
+                carry = 1;
+            }
+            else if (a % 2 == 0)
+            {
+                tempSum = tempSum * 10 + 1;
+                carry = 0;
+            }
+        }
+        else if (carry == 0)
+        {
+            if (a % 2 == 1)
+            {
+                tempSum = tempSum * 10 + 1;
+                carry = 0;
+            }
+            else if (a % 2 == 0)
+            {
+                tempSum = tempSum * 10 + 0;
+                carry = 0;
+            }
+        }
+        a /= 10;
+    }
+
+    // if b remains
+    while (b > 0) {
+        if(carry == 1) {
+            if (b % 2 == 1) {
+                tempSum = tempSum * 10 + 0;
+                carry = 1;
+            }
+            else if(b % 2 == 0) {
+                tempSum = tempSum * 10 + 1;
+                carry = 0;
+            }
+        }
+        else if (carry == 0) {
+            if (b % 2 == 1) {
+                tempSum = tempSum * 10 + 1;
+                carry = 0;
+            }
+            else if(b % 2 == 0) {
+                tempSum = tempSum * 10 + 0;
+                carry = 0;
+            }              
+        }   
+        b /= 10;     
+    }
+
+    // accounting for leftover carry
+    if(carry == 1) {
+        tempSum = tempSum * 10 + carry;
+        carry = 0;
+    }
+
+    // reversing the num
+    int sum = 0;
+    while (tempSum > 0) {
+        sum = sum * 10 + (tempSum % 10);
+        tempSum /= 10;
+    }
+
+    sum *= 10; 
+
+    return sum;
 }
 
 int main(int argc, char const *argv[])
 {
-    int x;
-    std ::cin >> x;
-    std ::cout << decimalToHexadecimal(x) << "\n";
-    
-    // x = int(x - 10 + 'A');
-    // std :: cout << x << "\n";
-    // char x2 = x;
-    // char x3 = char(x);
-    // std :: cout << x2 << "\n";
-    // std :: cout << x3 << "\n";
-    // std :: string s = std :: to_string(x);
-    // std :: cout << s << "\n";
-
+    int a,b;
+    std :: cin >> a >> b;
+    std :: cout << addBinary(a, b) << "\n";
 
     return 0;
 }
